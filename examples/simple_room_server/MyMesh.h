eac57f7de69a3b2cc2690dabd302f6f474d41391
Scott Powell <sqij@protonmail.com>
2026-03-20 12:32:41 +1100

    * version 1.14.1

--- examples/simple_room_server/MyMesh.h
+++ examples/simple_room_server/MyMesh.h
@@ -31,11 +31,11 @@
 /* ------------------------------ Config -------------------------------- */
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "06 Mar 2026"
+  #define FIRMWARE_BUILD_DATE   "26 Mar 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.14.0-mqtt-observer/chro-1"
+  #define FIRMWARE_VERSION   "v1.14.1-mqtt-observer/chro-1"
 #endif
 
 #ifndef LORA_FREQ
